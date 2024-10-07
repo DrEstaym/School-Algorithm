0eb083fcb1ac851dbe684513722fd037288169fe
Dr_Esta <progatorial@gmail.com>
2024-10-07 11:45:29 +0800

    Add InputTest

---
+++ InputTest/NumInputTest.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Declare Num var
+    int Num;
+
+    //Prompt to input
+    cout << "Insert Number: ";
+    cin >> Num;
+
+    //Output of the Num var
+    cout << "You number is: \n" << "\a" << Num << endl;
+}
