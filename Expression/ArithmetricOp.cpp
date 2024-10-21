4610e97826f9da334b4b02223a70fe6c3e34f1f3
Dr_Esta <progatorial@gmail.com>
2024-10-21 11:46:32 +0800

    added e and f variable

--- Expression/ArithmetricOp.cpp
+++ Expression/ArithmetricOp.cpp
@@ -2,16 +2,21 @@
 using namespace std;
 
 int main() {
-    int a, b, c, d;
+    int a, b, c, d, e;
+    float f;
 
     a = 5;
     b = a + 1;
     b = b + 5;
     c = (a + b) * 2 + a;
     d = c % b;
+    e = 8/3;                //with int
+    f = 8.0/3.0;            //with float
 
     cout << "a = " << a << endl;
     cout << "b = " << b << endl;
     cout << "c = " << c << endl;
     cout << "d = " << d << endl;
+    cout << "e = " << e << endl;
+    cout << "f = " << f << endl;
 }
