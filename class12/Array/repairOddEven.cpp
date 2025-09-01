3fe66f11f42fe5a1da9078b923ee8dc8275858bc
DrEstaym <progatorial@gmail.com>
2025-09-01 10:17:27 +0800

    LKPD 5

---
+++ class12/Array/repairOddEven.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    //Initiate Array
+    int size = 25;        //Tambahan: int size untuk mengatur jangkauan array-nya (biar enak dimodif)
+    int nilai[size];      //Kebanyakan isi (50 -> 25)
+    //Fill array
+    for(int i = 0; i < size; i++) {
+        nilai[i] = i + 1;
+    }
+
+    cout << "Bilangan genap: ";
+    for(int i = 0; i < size; i++) {
+        if((nilai[i] % 2) == 0) {
+            cout << nilai[i] << " ";
+        }
+    }
+
+    cout << "\nBilangan ganjil: ";
+    for(int i = 0; i  < size; i++) {
+        if((nilai[i] % 2) != 0) {
+            cout << nilai[i] << " ";
+        }
+    }
+}
